187eaef9932f0e6884603df3fa0a05f773607877
Cheolha Pedro Lee <leepedro@gmail.com>
2013-12-16 19:15:34 -0500

    created a repo

---
+++ Algorithms/test_algorithms.cpp
@@ -0,0 +1,47 @@
+#include <array>
+#include <vector>
+
+// Following function templates can be used for any standard container with iterators.
+// The first and second input argument can be different data type.
+
+// C = A + B
+template <typename InputIteratorA, typename InputIteratorB, typename OutputIterator>
+void Add(InputIteratorA it_a, InputIteratorA it_a_last, InputIteratorB it_b, OutputIterator it_c)
+{
+	for (; it_a != it_a_last; ++it_a, ++it_b, ++it_c)
+		*it_c = *it_a + *it_b;
+}
+
+// A += B
+template <typename InputIterator, typename InOutputIterator>
+void Add(InputIterator it_b, InputIterator it_b_last, InOutputIterator it_a)
+{
+	for (; it_b != it_b_last; ++it_a, ++it_b)
+		*it_a = *it_a + *it_b;
+}
+
+
+
+int main(void)
+{
+	std::array<int, 3> array1 = { 1, 2, 3 }, array2 = { 4, 5, 6 }, array3;
+	std::array<short, 3> array4 = { 4, 5, 6 };
+	std::vector<int> vector1 = { 1, 2, 3 }, vector2 = { 4, 5, 6 }, vector3(3);
+	std::vector<short>  vector4 = { 4, 5, 6 };
+
+	// InputIteratorA == InputIteratorB
+	Add(array1.cbegin(), array1.cend(), array2.cbegin(), array3.begin());	
+	Add(vector1.cbegin(), vector1.cend(), vector2.cbegin(), vector3.begin());
+
+	// InputIteratorA == InputIteratorB
+	Add(array1.cbegin(), array1.cend(), array2.begin());
+	Add(vector1.cbegin(), vector1.cend(), vector2.begin());
+
+	// InputIteratorA != InputIteratorB
+	Add(array1.cbegin(), array1.cend(), array4.cbegin(), array3.begin());
+	Add(vector1.cbegin(), vector1.cend(), vector4.cbegin(), vector3.begin());
+
+	// InputIteratorA != InputIteratorB
+	Add(array1.cbegin(), array1.cend(), array4.begin());
+	Add(vector1.cbegin(), vector1.cend(), vector4.begin());
+}
